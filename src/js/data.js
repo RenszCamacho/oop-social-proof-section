f7eb5a2ddf382b469bf96e225e3994fe18009a50
Renszo Camacho <varsovia84@gmail.com>
2021-01-30 21:07:37 +0100

    Data.js. Setted.

---
+++ src/js/data.js
@@ -0,0 +1,31 @@
+import colton from "../imges/image-colton.jpg";
+import irene from "../imges/image-irene.jpg";
+import anne from "../imges/image-anne.jpg";
+
+const user = {
+  colton: {
+    imege: colton,
+    fullName: "Colton Smith",
+    verified: true,
+    review:
+      "We needed the same printed design as the one we had ordered a week prior. Not only did they find the original order, but we also received it in time. Exellent!",
+  },
+
+  irene: {
+    imege: irene,
+    fullName: "Irene Roberts",
+    verified: true,
+    review:
+      "Customer service is always excellent and very quick turn around. Completely delighted with the simplicity of the purchase and the speed of delivery.",
+  },
+
+  anne: {
+    imege: anne,
+    fullName: "Anne Wallace",
+    verified: true,
+    review:
+      "Put an order with this company and can only praise them for the very high standard. Will definitely use them again and recommend them to everyone!",
+  },
+};
+
+export default user;
